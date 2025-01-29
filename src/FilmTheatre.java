6671f0c92b4c7ca0b731eb38ab7de1667f372421
Brandon Shea <brandon.shea1994@icloud.com>
2025-01-29 17:49:33 -0330

    basic switch logic

--- src/FilmTheatre.java
+++ src/FilmTheatre.java
@@ -9,6 +9,47 @@ public class FilmTheatre {
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        initSeats();
+
+        while (true) {
+            System.out.println("Film Theatre seat reservation system!");
+            System.out.println("1. Show seats");
+            System.out.println("2. Reserve a seat");
+            System.out.println("3. Cancel a reservation");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    showSeats();
+                    break;
+
+                case 2:
+                    reserveSeat(scanner);
+                    break;
+
+                case 3:
+                    cancelReservation(scanner);
+                    break;
+
+                case 4:
+                    System.out.println("Exiting the program...");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid. Try again.");
+            }
+        }
+    }
+
+    private static void initSeats() {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                seats[i][j] = 'O';
+            }
+        }
     }
 
 
