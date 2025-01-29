70d4d53505a31ef5558679b2420b42d2815cfd7e
Brandon Shea <brandon.shea1994@icloud.com>
2025-01-29 17:40:41 -0330

    basic variables and template

--- src/FilmTheatre.java
+++ src/FilmTheatre.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FilmTheatre {
+    private static int ROWS = 5;
+    private static int COLUMNS = 5;
+    private static char[][] seats = new char[ROWS][COLUMNS];
+
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
